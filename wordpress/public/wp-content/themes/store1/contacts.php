a3afaff1f10d2e8d437d4492ba48e3e898c14b75
starnekote <starc3001@gmail.com>
2025-05-26 07:03:38 +0300

    26/05/2025

--- wordpress/public/wp-content/themes/store1/contacts.php
+++ wordpress/public/wp-content/themes/store1/contacts.php
@@ -1,39 +1,4 @@
-<!DOCTYPE html>
-<html lang="ru">
-
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
-	<link rel="stylesheet" href="css/style.css">
-	<title>Контакты</title>
-</head>
-
-<body>
-	<!-- Меню -->
-	<div class="menu">
-		<div class="container">
-			<div class="row">
-				<div class="logo">
-					<a href="index.html">
-						<img src="img/logo.png" alt="логотип">
-					</a>
-				</div>
-				<ul>
-					<li><a href="index.html">Главная</a></li>
-					<li><a href="about.html">О компании</a></li>
-					<li><a href="catalog.html">Каталог</a></li>
-					<li><a href="order.html">На заказ</a></li>
-					<li><a href="portfolio.html">Наши работы</a></li>
-					<li><a href="contacts.html">Контакты</a></li>
-				</ul>
-				<div class="phone">
-					<a href="tel:+79186687673">&#9742; +7 (918) 668-76-73</a>
-				</div>
-			</div>
-		</div>
-	</div>
+<?php get_header(); ?>
   <!-- Контакты -->
   <div class="our-contacts">
     <div class="container">
--- wordpress/public/wp-content/themes/store1/templates/policy.php
+++ wordpress/public/wp-content/themes/store1/templates/policy.php
@@ -4,7 +4,7 @@ Template Name: Політика конфіденційності
 */
 get_header(); ?>
   <!-- Текст политики конфиденциальности -->
-  <div class="policy" style="background-image: url(<?php echo get_field('background_policy_name') ?>)">
+  <div class="policy" style="background-image: url(<?php echo pods_field_display('background_image') ?>)">
     <div class="container">
       <div class="row">
         <h2><?php the_title() ?></h2>
