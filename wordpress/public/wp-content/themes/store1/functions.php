3c0d8bb1588ef2b9eba3ceefb0eb42d5eaf9a436
starnekote <starc3001@gmail.com>
2025-06-02 16:43:11 +0300

    додав кастомний тип постов

--- wordpress/public/wp-content/themes/store1/functions.php
+++ wordpress/public/wp-content/themes/store1/functions.php
@@ -2,6 +2,7 @@
     add_action('wp_enqueue_scripts', 'add_styles');
     add_action('after_setup_theme', 'add_features');
     add_action('after_setup_theme', 'add_menu');
+    add_action( 'init', 'register_post_types' );
     
         function add_styles() {
             if(is_page_template('templates/portfolio.php')) {
@@ -30,4 +31,51 @@
             register_nav_menu('bottom', 'Політика конфіденційності');
          }
          remove_filter('template_redirect', 'redirect_canonical');
+
+function register_post_types(){
+
+ register_post_type( 'post_type_name', [
+  'label'  => null,
+  'labels' => [
+   'name'               => 'Товари', // основное название для типа записи
+   'singular_name'      => 'Товар', // название для одной записи этого типа
+   'add_new'            => 'Додати товар', // для добавления новой записи
+   'add_new_item'       => 'Додавання товарыв', // заголовка у вновь создаваемой записи в админ-панели.
+   'edit_item'          => 'Редагувати товар', // для редактирования типа записи
+   'new_item'           => 'Новое ____', // текст новой записи
+   'view_item'          => 'Смотреть ____', // для просмотра записи этого типа.
+   'search_items'       => 'Искать ____', // для поиска по этим типам записи
+   'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
+   'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
+   'parent_item_colon'  => '', // для родителей (у древовидных типов)
+   'menu_name'          => 'Товари', // название меню
+  ],
+  'description'            => '',
+  'public'                 => true,
+  // 'publicly_queryable'  => null, // зависит от public
+  // 'exclude_from_search' => null, // зависит от public
+  // 'show_ui'             => null, // зависит от public
+  // 'show_in_nav_menus'   => null, // зависит от public
+  'show_in_menu'           => null, // показывать ли в меню админки
+  // 'show_in_admin_bar'   => null, // зависит от show_in_menu
+  'show_in_rest'        => true, // добавить в REST API. C WP 4.7
+  'rest_base'           => null, // $post_type. C WP 4.7
+  'menu_position'       => 5,
+  'menu_icon'           => 'dashicons-portfolio',
+  'capability_type'   => 'page',
+  //'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
+  //'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
+  'hierarchical'        => true,
+  'supports'            => [ 'title', 'editor', 'custom-fields'], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+  'taxonomies'          => ['category'],
+  'has_archive'         => true,
+  'rewrite'             => true,
+  'query_var'           => true,
+ ] );
+
+}
+
+
+
 ?>
+
